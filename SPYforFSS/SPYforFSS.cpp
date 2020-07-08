dbbaaff6462c830c6dc9cca99cf976523b166359
sik0813 <newlcb0813@naver.com>
2020-07-08 04:51:46 +0000

    system 경로 GetSystemDirectoryW 함수이용 하여 가져오는 방식으로 변경

--- SPYforFSS/SPYforFSS.cpp
+++ SPYforFSS/SPYforFSS.cpp
@@ -2,22 +2,6 @@
 #include "CMainDlg.h"
 #include "../MessageSYP/MessageSPY.h"
 
-
-BOOL Is64BitWindows()
-{
-#if defined(_WIN64)
-	return TRUE;  // 64-bit programs run only on Win64
-#elif defined(_WIN32)
-	// 32-bit programs run on both 32-bit and 64-bit Windows
-	// so must sniff
-	BOOL f64 = FALSE;
-	return IsWow64Process(GetCurrentProcess(), &f64) && f64;
-#else
-	return FALSE; // Win64 does not support Win16
-#endif
-}
-
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
 	BOOL f64 = FALSE;
@@ -36,9 +20,11 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		otherBitQuitEvent = CreateEventW(NULL, TRUE, TRUE, L"otherBitDllQuit");
 		ResetEvent(otherBitQuitEvent);
 
-		WCHAR rundllPath[] = L"C:\\Windows\\System32\\";
-		WCHAR programPath[_MAX_PATH];
-		GetModuleFileName(NULL, programPath, _MAX_PATH);
+		WCHAR rundllPath[MAX_PATH] = { 0, };
+		GetSystemDirectoryW(rundllPath, MAX_PATH);
+
+		WCHAR programPath[MAX_PATH];
+		GetModuleFileName(NULL, programPath, MAX_PATH);
 		std::wstring paramString = L"\"" + std::wstring(programPath);
 		paramString = paramString.substr(0, paramString.find_last_of(L'\\') + 1);
 		paramString += L"MessageSPY_amd64.dll\" SpyStart";
